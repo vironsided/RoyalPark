4224cdaaa52d474e396c7db905aa35e9de4ab9b8
Anar <anar.ismahilov@gmail.com>
2025-12-08 12:51:21 +0100

    Initial commit

---
+++ Application/Backend/app/config.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class Settings(BaseModel):
+    # Подключение к БД (из задачи):
+    PG_HOST: str = "127.0.0.1"
+    PG_PORT: int = 5432
+    PG_USER: str = "postgres"
+    PG_PASSWORD: str = "admin"
+    PG_DB: str = "fastApiAyaz"
+
+    # Секреты/куки:
+    SESSION_SECRET_KEY: str = "change-this-in-production"
+    COOKIE_NAME: str = "session_id"
+
+    # Root-учётка из ТЗ:
+    ROOT_USERNAME: str = "root"
+    ROOT_PASSWORD: str = "admin Ayaz"
+
+
+settings = Settings()
